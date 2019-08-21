748f5e2fa56e62b493337e7270b238020fbdb92c
Musideen Dauda <muziyindojava@gmail.com>
2019-08-21 18:27:02 +0100

    many things done

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,14 @@ class Blog(models.Model):
 	body = models.TextField(max_length=1000)
 	image = models.ImageField(upload_to="image/blogimages/")
 
+	#returns specified text chunk from body
+	#nb: when you define a functon in a class you must pass a self parameter into it
+	def summary(self):
+		return self.body[:100] #extract first 100 characters
+
+	def __str__(self):
+		return self.title #show titles in python admin page for blog model
+
+
 	'''NB: anytime you make changes on a model such as renaming a field or so, you will need to makemigrations
        and then migrate'''
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404 # used to get data from the db or return 404 if data is not found
+from .models import Blog
 
 # Create your views here.
+
+def allBlogs(request):
+	posts = Blog.objects
+	return render(request,'blog/allblogs.html',{'posts':posts})
+
+def detail(request,blog_id):
+	detailblog = get_object_or_404(Blog, pk=blog_id)
+	return render(request,'blog/detail.html',{'detail':detailblog})
